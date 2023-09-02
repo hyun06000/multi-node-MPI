f169f3fb11bd22c8b49672d83050530ede71b76b
David Sang-hyun Park <hyun06000@gmail.com>
2023-09-02 16:40:25 +0100

    [feature] ssh connection without passphrase

---
+++ copy-ssh-pubkey.sh
@@ -0,0 +1,3 @@
+ssh pi@192.168.0.12 -t "ssh-keygen && ssh-copy-id pi@192.168.0.11"
+ssh pi@192.168.0.13 -t "ssh-keygen && ssh-copy-id pi@192.168.0.11"
+ssh pi@192.168.0.14 -t "ssh-keygen && ssh-copy-id pi@192.168.0.11"
--- install-mpi.sh
+++ install-mpi.sh
@@ -6,3 +6,9 @@ sudo apt-get install -y mpich libmpich-dev
 # 설치 확인
 mpicc --version
 mpiexec --version
+
+
+ssh pi@192.168.0.12 -t "sudo apt-get update && sudo apt-get install -y mpich libmpich-dev"
+ssh pi@192.168.0.13 -t "sudo apt-get update && sudo apt-get install -y mpich libmpich-dev"
+ssh pi@192.168.0.14 -t "sudo apt-get update && sudo apt-get install -y mpich libmpich-dev"
+
